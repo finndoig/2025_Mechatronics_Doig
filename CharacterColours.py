78a3bed391d9c5e4e61a03086df8f330a37f114b
Finn Doig <fd445@bath.ac.uk>
2025-11-24 21:58:19 +0000

    fix: :bug: getting correct colour limits
    testing and checking that camera picks up correct characters and only that character

--- CharacterColours.py
+++ CharacterColours.py
@@ -9,10 +9,11 @@ def get_characters():
 	"""
 
 	# Arrays for the rgb ranges of each character
-	Character1 = [57,  141, 188, 77, 161, 208]   # r_min, g_min, b_min, r_max, g_max, b_max
-	Character2 = [235, 180, 61, 255, 200, 81]
-	Character3 = [186, 139, 233, 206, 159, 253]
-	Character4 = [221, 239, 93, 241, 255, 113]
+	# Character1 = [37,  121, 168, 97, 181, 228]   # r_min, g_min, b_min, r_max, g_max, b_max
+	Character1 = [0, 20, 70, 30, 80, 150] # finn
+	Character2 = [170, 80, 45, 230, 150, 105] # jake
+	Character3 = [90, 20, 30, 170, 70, 80] # lsp
+	Character4 = [140, 125, 60, 180, 165, 100] # tree trunk
 
 	Characters = [Character1, Character2, Character3, Character4]
 	return Characters
